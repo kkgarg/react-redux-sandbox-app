c1bdf9e38b6d4da259bc630426fc2e131266a765
kkgarg <kamalkgarg@hotmail.com>
2016-10-24 23:39:24 +0000

    Signup form added

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -4,14 +4,33 @@ import * as actions from '../../actions';
 
 class Signup extends Component {
   render() {
+
+    const { handleSubmit, fields: {email, password, password_confirmation}} = this.props;
+
     return(
-      <div>
-        Signup
-      </div>
+        <form action="">
+          <fieldset className="form-group">
+            <label>Email:</label>
+            <input className="form-control" {...email} />
+          </fieldset>
+          <fieldset className="form-group">
+            <label>Password:</label>
+            <input type="password" className="form-control" {...password} />
+          </fieldset>
+          <fieldset className="form-group">
+            <label>Confirm Password:</label>
+            <input type="password" className="form-control" {...password_confirmation} />
+          </fieldset>
+          <button type="submit" className="btn btn-primary">Sign up</button>
+        </form>
     );
+
   }
 }
 
-export default Signup;
+export default reduxForm({
+  form: "signup",
+  fields: ["email", "password", "password_confirmation"]
+})(Signup);
 
 
